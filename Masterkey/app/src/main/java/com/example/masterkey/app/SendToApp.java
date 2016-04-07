d0702c3ff75bc4fc4a7c6659b34486ab62db3019
Steven Ly <Rauiy@bitbucket.org>
2016-04-07 15:49:47 +0200

    added fingerprint code

---
+++ Masterkey/app/src/main/java/com/example/masterkey/app/SendToApp.java
@@ -0,0 +1,19 @@
+package com.example.masterkey.app;
+
+import android.content.Intent;
+import android.net.Uri;
+
+/**
+ * Created by Steven on 2016-04-07.
+ */
+public class SendToApp {
+
+    public SendToApp(){
+
+    }
+
+    public void test(){
+
+        
+    }
+}
